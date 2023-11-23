bd64c174440bda442ee71cb504526567cb87cc7d
Aslan <jasmina031015@gmail.com>
2023-11-23 11:33:36 +0300

    added Templates(symfony twig/pack),(symfony debug/pack), (symfony/asset), (symfony/webpack-encore), debug:autowiring, debug:autowiring log, debug:autowiring twig, debug:autowiring route, node_modules, bootstrap, Axios.

---
+++ src/Controller/VinylController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+use function Symfony\Component\String\u;
+
+class VinylController extends AbstractController
+{
+    #[Route('/', name: 'app_homepage')]
+    public function homepage(): Response
+    {
+
+
+
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Watterfalls', 'artist' => 'TLS'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose','artist' => ' Seal'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+
+        ];
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks,
+        ]);
+
+
+    }
+
+
+    #[Route('/browse/{slug}', name: 'app_browse')]
+    public function browse(string $slug = null): Response
+    {
+
+
+        $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
+
+        return $this->render('vinyl/browse.html.twig', [
+            'genre' => $genre
+        ]);
+    }
+}
